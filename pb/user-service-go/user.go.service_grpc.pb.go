9a3041e99039417ac365705d6bd54702da743e31
Husky <husky.robot.dog@gmail.com>
2022-03-01 12:26:29 +0800

    init

--- pb/user-service-go/user.go.service_grpc.pb.go
+++ pb/user-service-go/user.go.service_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type UserServiceClient interface {
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResult, error)
+	TestResult(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*CommonResult, error)
 }
 
 type userServiceClient struct {
@@ -42,11 +43,21 @@ func (c *userServiceClient) Login(ctx context.Context, in *LoginRequest, opts ..
 	return out, nil
 }
 
+func (c *userServiceClient) TestResult(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*CommonResult, error) {
+	out := new(CommonResult)
+	err := c.cc.Invoke(ctx, "/husky_demos.user_service_go.v1.UserService/testResult", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations should embed UnimplementedUserServiceServer
 // for forward compatibility
 type UserServiceServer interface {
 	Login(context.Context, *LoginRequest) (*LoginResult, error)
+	TestResult(context.Context, *LoginRequest) (*CommonResult, error)
 }
 
 // UnimplementedUserServiceServer should be embedded to have forward compatible implementations.
@@ -55,6 +66,9 @@ type UnimplementedUserServiceServer struct {
 
 func (UnimplementedUserServiceServer) Login(context.Context, *LoginRequest) (*LoginResult, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (UnimplementedUserServiceServer) TestResult(context.Context, *LoginRequest) (*CommonResult, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method TestResult not implemented")
 }
 
 // UnsafeUserServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -86,6 +100,24 @@ func _UserService_Login_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_TestResult_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(LoginRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).TestResult(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/husky_demos.user_service_go.v1.UserService/testResult",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).TestResult(ctx, req.(*LoginRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -96,6 +128,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "login",
 			Handler:    _UserService_Login_Handler,
+		},
+		{
+			MethodName: "testResult",
+			Handler:    _UserService_TestResult_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- service/user-service-go.go
+++ service/user-service-go.go
@@ -3,6 +3,7 @@ package service
 import (
 	context "context"
 	"github.com/jmoiron/sqlx"
+	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"user-service-go/model"
 	"user-service-go/pb/user-service-go"
@@ -39,5 +40,36 @@ func (s *UserService) Login(ctx context.Context, param *v1.LoginRequest) (*v1.Lo
 		},
 		Token:      "123",
 		ExpireTime: timestamppb.Now(),
+	}, nil
+}
+
+func (s *UserService) TestResult(ctx context.Context, param *v1.LoginRequest) (*v1.CommonResult, error) {
+	if param.LoginName == "" {
+		return nil, utils.NewError(1, "登录账号不能为空")
+	}
+	if param.LoginPass == "" {
+		return nil, utils.NewError(2, "登录密码不能为空")
+	}
+	var user model.User
+	if err := s.db.Get(&user, "select * from users where login_name=? and login_pass=?", param.LoginName, param.LoginPass); err != nil {
+		return nil, utils.NewError(3, "用户不存在")
+	}
+	result, err := anypb.New(&v1.LoginResult{
+		User: &v1.User{
+			Id:        user.Id,
+			NickName:  user.NickName,
+			LoginName: user.LoginName,
+			IsLocking: user.IsLocking,
+		},
+		Token:      "123",
+		ExpireTime: timestamppb.Now(),
+	})
+	if err != nil {
+		return nil, utils.NewError(4, "业务异常")
+	}
+	return &v1.CommonResult{
+		Code:    0,
+		Message: "",
+		Data:    result,
 	}, nil
 }
